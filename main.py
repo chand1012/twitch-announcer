d71b170f1363699d7da6f82c71304606ea26d425
chand1012 <chandler@chand1012.dev>
2021-07-23 20:46:48 +0000

    Add improved error handling

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import time
 
-import twitch
 import dotenv
+import requests
+import tweepy
+import twitch
 
-from hashtags import get_hashtags, append_hashtag
-from discord_hook import send_hook as discord_hook
 import twitter
+from discord_hook import send_hook as discord_hook
+from hashtags import append_hashtag, get_hashtags
 
 print('Initializing....')
 
@@ -14,11 +16,9 @@ dotenv.load_dotenv()
 
 # set to 10 for faster debugging
 # make this an environment variable at some point
-WAIT = 60 
+WAIT = 60*3
 
 STREAMER = os.getenv('TWITCH_USERNAME')
-
-twitch_client = twitch.Helix(client_id=os.getenv('TWITCH_CLIENT_ID'), client_secret=os.getenv('TWITCH_CLIENT_SECRET'))
 
 isLive = False
 
@@ -29,7 +29,13 @@ while True:
 
     twitter_hashtags = ''
 
-    streamer = twitch_client.user(STREAMER)
+    try:
+        twitch_client = twitch.Helix(client_id=os.getenv('TWITCH_CLIENT_ID'), client_secret=os.getenv('TWITCH_CLIENT_SECRET'))
+        streamer = twitch_client.user(STREAMER)
+    except requests.exceptions.HTTPError:
+        print('There was an error. Waiting and retrying...')
+        time.sleep(WAIT)
+        continue
 
     currentlyLive = streamer.is_live
 
@@ -51,9 +57,19 @@ while True:
         # post to discord & twitter
         if os.getenv('DISCORD_WEBHOOK_URL'):
             print("Posting to Discord....")
-            discord_hook(url, thumbnail, title, STREAMER, avatar, '9147FF')
+            while True:
+                try:
+                    discord_hook(url, thumbnail, title, STREAMER, avatar, '9147FF')
+                except requests.exceptions.HTTPError:
+                    print('There was an error trying to post to discord, waiting and retrying...')
+                    time.sleep(60)
+                    continue
+                break
         print("Posting to Twitter....")
-        twitter.make_post(title, url, twitter_hashtags)
+        try:
+            twitter.make_post(title, url, twitter_hashtags)
+        except tweepy.error.TweepError:
+            print('Duplicate tweet, skipping.') 
 
     if not currentlyLive and isLive:
         isLive = False
